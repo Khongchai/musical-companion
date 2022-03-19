74479c8568876d4623b8a4eb5d89b9690fc11a5c
k.greesuradej <world1955@hotmail.com>
2022-03-19 15:38:29 +0700

    updated button for mock payment amount

--- src/utils-hooks/usePaypal.ts
+++ src/utils-hooks/usePaypal.ts
@@ -22,11 +22,8 @@ export default function useScript(
               purchase_units: [
                 {
                   amount: {
-                    //TODO uncomment for production
-                    value: 7,
-                    currency_code: "USD",
-                    // value: totalToPay,
-                    // currentcy_code: "USD",
+                    value: totalToPay,
+                    currentcy_code: "USD",
                   },
                 },
               ],
